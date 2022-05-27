23267343ebb36b74e20ef824af0647b9b511ee3b
ikbalsazib <ikbal.sazib@gmail.com>
2022-05-27 14:26:34 +0600

    Event & Outlet Change

--- I Am Rich/ViewController.swift
+++ I Am Rich/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(named: "DiceOne")
+        diceImageView2.image = UIImage(named: "DiceSix")
+        
     }
 
-
+    @IBAction func onRollBtnClick(_ sender: UIButton) {
+        print("Button Clicked")
+        diceImageView1.image = UIImage(named: "DiceTwo")
+        diceImageView2.image = UIImage(named: "DiceFive")
+    }
+    
 }
 
